062af35e96e75b598c85a4d845e3cc5df85c0fa7
Renay Earnshaw <renayearnshaw@gmail.com>
2026-01-19 09:49:39 +0000

    gitignore ignores credentials

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -4,7 +4,7 @@ async function testAuth() {
   const authService = new AuthService();
 
   // Log the user in and retrieve a JWT ID token
-  await authService.login('USER-NAME', 'PASSWORD');
+  await authService.login('barneyearnshaw', 'o%z0&jOO2p@1H2J');
 
   const idToken = await authService.getIdToken();
   console.log(idToken);
